97e8321c68fa7e2a9bf6a34e935a87843b74a1f0
Beverly <duran.beverly@gmail.com>
2023-06-05 23:24:14 -0400

    fixed landing page css

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -26,64 +26,76 @@ export default function LandingPage() {
 
     return (
         <>
-            <ul className='top-nav'>
-                <li className='top-left-nav'>
-                    <i className={`fa-solid fa-comment-dots`}></i>
-                    <NavLink exact to="/" className="discordia-title">Discordia</NavLink>
-                </li>
-                <li>
-                    <OpenModalButton
-                        buttonText="Log In"
-                        // onItemClick={closeMenu}
-                        className="login-button"
-                        modalComponent={<LoginFormModal />}
-                    />
-                </li>
-                <li>
-                    <OpenModalButton
-                        buttonText="Sign Up"
-                        // onItemClick={closeMenu}
-                        className="sign-up-button"
-                        modalComponent={<SignupFormModal />}
-                    />
-                </li>
-                {/* {isLoaded && (
+            <div id="root">
+
+                <ul className='top-nav'>
+                    <li className='top-left-nav'>
+                        <i className={`fa-solid fa-comment-dots`}></i>
+                        <NavLink exact to="/" className="discordia-title">Discordia</NavLink>
+                    </li>
+                    <li className="top-right-nav">
+
+                        <OpenModalButton
+                            buttonText="Log In"
+                            // onItemClick={closeMenu}
+                            className="login-button"
+                            modalComponent={<LoginFormModal />}
+                        />
+
+                        <OpenModalButton
+                            buttonText="Sign Up"
+                            // onItemClick={closeMenu}
+                            className="sign-up-button"
+                            modalComponent={<SignupFormModal />}
+                        />
+
+
+                    </li>
+                    {/* {isLoaded && (
                     <li className='top-right-nav'>
                         <ProfileButton user={user} />
                         <i className={`fa-solid fa-bars`}></i>
                     </li>
                 )} */}
-            </ul>
+                </ul>
 
-            <div className="wrapper">
-                <div className="body-wrapper">
-                    < div className="body">
-                        <h1 className="landing-title">This is the Place for You...</h1>
-                        <span className="landing-body">...Connect, Communicate, Community: Unite and Engage in Togetherness. we believe in the power of togetherness, meaningful conversations, and fostering a vibrant community. Our platform is designed to bring people closer, allowing them to belong to various groups, whether it's a school club, a gaming community, or a worldwide art collective.</span>
-                        {user ? (
-                            <button onClick={handleClick} className="open-discordia">Open Discordia in your browser</button>
 
-                        ) : (
-                            <OpenModalButton
-                                buttonText="Open Discordia in your browser"
-                                // onItemClick={closeMenu}
-                                className="open-discordia"
-                                modalComponent={<LoginFormModal />}
-                            />
-                        )
-                        }
+                <div className="wrapper">
+                    <div className="body-wrapper">
+                        < div className="body">
+                            <h1 className="landing-title">This is the Place for You...</h1>
+                            <span className="landing-body">...Connect, Communicate, Community: Unite and Engage in Togetherness. we believe in the power of togetherness, meaningful conversations, and fostering a vibrant community. Our platform is designed to bring people closer, allowing them to belong to various groups, whether it's a school club, a gaming community, or a worldwide art collective.</span>
+                            {user ? (
+                                <button onClick={handleClick} className="open-discordia">Open Discordia in your browser</button>
+
+                            ) : (
+                                <OpenModalButton
+                                    buttonText="Open Discordia in your browser"
+                                    // onItemClick={closeMenu}
+                                    className="open-discordia"
+                                    modalComponent={<LoginFormModal />}
+                                />
+                            )
+                            }
+                        </div>
                     </div>
+
+                    <div className="images-all">
+
+                        <img src={background} className="main-bg-image" alt="clouds"></img>
+                        <img src={backgroundRight} className="right-bg-image" alt="few cartoon people hanging out" ></img>
+                        <img src={backgroundLeft} className="left-bg-image" alt="cartoon people hanging out"></img>
+                    </div>
+
+
+
+
+                    <div className="footer"></div>
+
                 </div>
-                <div className="all-images">
-                    <img src={background} className="main-bg-image" alt="clouds"></img>"
-                    <img src={backgroundRight} className="right-bg-image" alt="few cartoon people hanging out" ></img>"
-                    <img src={backgroundLeft} className="left-bg-image" alt="cartoon people hanging out"></img>"
+                <div>
 
-                    {/* <img src={backgroundMain} className="main-bg-image" ></img>" */}
                 </div>
-
-                <div className="footer"></div>
-
             </div>
         </>
     )
